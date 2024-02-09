2964451204b521a6553e5825637e01d274d7dc79
Rodney <54865844+rmartin-sc@users.noreply.github.com>
2024-02-09 02:40:00 +0000

    Changes

---
+++ docs/script.js
@@ -0,0 +1,11 @@
+console.log("hi from the script file");
+
+function add(a, b) {
+    for ( let i = 0; i < a; i += 1) {
+        console.log(i);
+    }
+}
+
+var sum = add(123, 456);
+
+console.log(sum);
