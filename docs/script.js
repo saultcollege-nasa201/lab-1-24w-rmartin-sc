1081b7f2359297f322127605cf45fc804fd6d699
Rodney <54865844+rmartin-sc@users.noreply.github.com>
2024-02-10 12:09:00 +0000

    Final solution

--- docs/script.js
+++ docs/script.js
@@ -11,3 +11,17 @@ var sum = add(123, 456);
 console.log(sum);
 
 // This is a change I made
+
+document.forms.contactForm.addEventListener('submit', (event) => {
+
+    let catInput = document.getElementById('cat');
+    let dogInput = document.getElementById('dog');
+    let otherInput = document.getElementById('other');
+
+    if ( ! catInput.checked && ! dogInput.checked && ! otherInput.checked ) {
+        document.getElementById('radioError').textContent = "You need to pick one of these"
+        document.getElementById('catLabel').style.color = 'red';
+        event.preventDefault();
+        return false;
+    }
+});
